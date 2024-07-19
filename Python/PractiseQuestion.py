0dc51587168763415c418f7c615e0e811476eaa6
Naman-Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-07-19 16:43:44 -0400

    code updated

---
+++ Python/PractiseQuestion.py
@@ -0,0 +1,23 @@
+info = {
+    "table" : ["a pievce of furntiure", "list of facts and figure"] ,
+    "cat" : "a small animal"
+}
+print(info)
+
+# dictionary = {}
+# print("Enter 3 subject with their marks")
+# subject1 = input("Enter a Subject name: ")
+# marks1  = int(input("Enter marks obtained in subject"))
+# dictionary.update({subject1: marks1})
+# subject2 = input("Enter a Subject name: ")
+# marks2  = int(input("Enter marks obtained in subject"))
+# dictionary.update({subject2: marks2})
+# subject3 = input("Enter a Subject name: ")
+# marks3  = int(input("Enter marks obtained in subject"))
+# dictionary.update({subject3: marks3})
+
+# print("dictionary print ", dictionary)
+
+set = {9, "9"}
+
+print(set)
---
+++ Python/Union.py
@@ -0,0 +1,5 @@
+set = {1,2,3}
+set1 = {3, 5,6,7}
+
+print(set.union(set1))
+print(set.intersection(set1))
